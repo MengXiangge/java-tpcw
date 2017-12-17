8ec0d7bd53107c6b7990ff52ba2e2cc375c5194d
thomas.liao256 <thomas.liao256@gmail.com>
2017-12-17 02:38:48 +0000

    first commit

--- tpcw/servlets/TPCW_home_interaction.java
+++ tpcw/servlets/TPCW_home_interaction.java
@@ -65,28 +65,48 @@ public class TPCW_home_interaction extends HttpServlet {
       Vector column1 = new Vector();
       Vector column2 = new Vector();
 
-      column1.addElement(new String("HP"));//1
-      column2.addElement(new String("Dell"));
-      column1.addElement(new String("Apple"));//2
-      column2.addElement(new String("Asus"));
-      column1.addElement(new String("Acer"));//3
-      column2.addElement(new String("Lenovo"));
-      column1.addElement(new String("Alienware"));//4
-      column2.addElement(new String("Sony"));
-      column1.addElement(new String("Panasonic"));//5
-      column2.addElement(new String("Toshiba"));
-      column1.addElement(new String("Sansung"));//6
-      column2.addElement(new String("Compaq"));
-      column1.addElement(new String("Microsoft"));//7
-      column2.addElement(new String("Sager"));
-      column1.addElement(new String("LG"));//8
-      column2.addElement(new String("CyberPower PC"));
-      column1.addElement(new String("Google"));//9
-      column2.addElement(new String("Intel"));
-      column1.addElement(new String("MSI"));//10
-      column2.addElement(new String("RCA"));
-      column1.addElement(new String("Gateway"));//11
-      column2.addElement(new String("Bit"));
+      //column1.addElement(new String("HP"));//1
+      //column2.addElement(new String("Dell"));
+      //column1.addElement(new String("Apple"));//2
+      //column2.addElement(new String("Asus"));
+      //column1.addElement(new String("Acer"));//3
+      //column2.addElement(new String("Lenovo"));
+      //column1.addElement(new String("Alienware"));//4
+      //column2.addElement(new String("Sony"));
+      //column1.addElement(new String("Panasonic"));//5
+      //column2.addElement(new String("Toshiba"));
+      //column1.addElement(new String("Sansung"));//6
+      //column2.addElement(new String("Compaq"));
+      //column1.addElement(new String("Microsoft"));//7
+      //column2.addElement(new String("Sager"));
+      //column1.addElement(new String("LG"));//8
+      //column2.addElement(new String("CyberPower PC"));
+      //column1.addElement(new String("Google"));//9
+      //column2.addElement(new String("Intel"));
+      //column1.addElement(new String("MSI"));//10
+      //column2.addElement(new String("RCA"));
+      //column1.addElement(new String("Gateway"));//11
+      //column2.addElement(new String("Bit"));
+      column1.addElement(new String("ARTS"));//1
+      column2.addElement(new String("NON-FICTION"));
+      column1.addElement(new String("BIOGRAPHIES"));//2
+      column2.addElement(new String("PARENTING"));
+      column1.addElement(new String("BUSINESS"));//3
+      column2.addElement(new String("POLITICS"));
+      column1.addElement(new String("CHILDREN"));//4
+      column2.addElement(new String("REFERENCE"));
+      column1.addElement(new String("COMPUTERS"));//5
+      column2.addElement(new String("RELIGION"));
+      column1.addElement(new String("COOKING"));//6
+      column2.addElement(new String("ROMANCE"));
+      column1.addElement(new String("HEALTH"));//7
+      column2.addElement(new String("SELF-HELP"));
+      column1.addElement(new String("HISTORY"));//8
+      column2.addElement(new String("SCIENCE-NATURE"));
+      column1.addElement(new String("HOME"));//9
+      column2.addElement(new String("SCIENCE-FICTION"));
+      column1.addElement(new String("HUMOR"));//10
+column2.addElement(new String("SPORTS"));
 
       /*SERVLET SETUP*/
       HttpSession session = req.getSession(false);
@@ -103,9 +123,9 @@ public class TPCW_home_interaction extends HttpServlet {
       String SHOPPING_ID = req.getParameter("SHOPPING_ID");
 
       //Generate Home Page Head
-      out.print("<HTML> <HEAD> <TITLE>TPC-W Home Page</TITLE></HEAD>\n");
+      out.print("<HTML> <HEAD> <TITLE>Laptop Trading and Information Platform</TITLE></HEAD>\n");
       out.print("<BODY BGCOLOR=\"#ffffff\">\n"); 
-      out.print("<H1 ALIGN=\"center\">TPC Web Commerce Benchmark (TPC-W)</H1>\n");
+      out.print("<H1 ALIGN=\"center\">Laptop Trading and Information Platform</H1>\n");
       out.print("<P ALIGN=\"CENTER\">\n");
       out.print("<IMG SRC=\"../tpcw/Images/tpclogo.gif\" ALIGN=\"BOTTOM\""+ 
 		"BORDER=\"0\" WIDTH=\"288\" HEIGHT=\"67\"></P>\n");
@@ -226,8 +246,8 @@ public class TPCW_home_interaction extends HttpServlet {
       //Generate Trailer
       out.print("<hr><font size=-1>\n");
       out.print("<a href=\"http://www.tpc.org/miscellaneous/TPC_W.folder/Company_Public_Review.html\">TPC-W Benchmark</a>,\n"); 
-      out.print("<a href=\"http://www.cae.wisc.edu/~mikko/ece902.html\">ECE 902</a>,\n");
-      out.print("<a href=\"http://www.cs.wisc.edu/~arch/uwarch\">University of Wisconsin Computer Architecture</a>,November 1999.\n");
+      out.print("<a href=\"http://www.google.com\">ECE 902</a>,\n");
+      out.print("<a href=\"http://www.google.com\">Computer Laptop Trading and Information Platform</a>,December 2017.\n");
       out.print("</font> </BODY> </HTML>\n");
       out.close();
       return;
